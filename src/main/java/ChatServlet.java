c693cc3a79548bc6e9dd44a34ccd0c33b8c9aa1e
alshimaaelnady <alshimaaelnady@gmail.com>
2020-04-07 08:37:12 +0200

    first commit

---
+++ src/main/java/ChatServlet.java
@@ -0,0 +1,38 @@
+
+import com.google.gson.Gson;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+public class ChatServlet extends HttpServlet {
+    static List<Message> messages = new ArrayList<>();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println(builGsonFromObject());
+
+    }
+
+    private String builGsonFromObject(){
+        Gson gsonMessage = new Gson();
+        return gsonMessage.toJson(messages);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Message message = new Message();
+        message.setSender(request.getParameter("sender"));
+        message.setText(request.getParameter("text"));
+        messages.add(message);
+
+    }
+}
---
+++ src/main/java/Message.java
@@ -0,0 +1,18 @@
+public class Message {
+   private String sender;
+   private String text;
+    public void setSender(String sender) {
+        this.sender=sender;
+    }
+
+    public void setText(String text) {
+        this.text=text;
+    }
+    public String getSender() {
+        return sender;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
